c1c9b5b2af37d97d6338a6d6c5e233816b77833a
shabbir <sbr.sk44@gmail.com>
2021-11-07 20:36:28 +0530

    lets see

---
+++ static/src/main/java/IDGroup/App.java
@@ -0,0 +1,13 @@
+package IDGroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
